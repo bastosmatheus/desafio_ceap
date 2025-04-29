739a98f47788b089ce3c9028f3f185f71cd254d2
matheus <matheusbastosandrade@gmail.com>
2025-04-29 18:26:42 -0300

    feat: adicionando migrations para alterar o banco de dados

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,17 +1,22 @@
 import { config } from 'dotenv';
 import { DataSource } from 'typeorm';
 import { Aluno } from 'src/modules/aluno/entities/aluno.entity';
+import { Matricula } from 'src/modules/matricula/entities/matricula.entity';
+import { Curso } from 'src/modules/curso/entities/curso.entity';
+import { ConfigService } from '@nestjs/config';
 
 config();
 
+const configService = new ConfigService();
+
 const dataSource = new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST, // Endereço do servidor DB
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME, // Usuário de dev
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  entities: [Aluno],
+  host: configService.get<string>('DB_HOST'),
+  port: configService.get<number>('DB_PORT'),
+  username: configService.get<string>('DB_USER'),
+  password: configService.get<string>('DB_PASSWORD'),
+  database: configService.get<string>('DB_DATABASE'),
+  entities: [Aluno, Matricula, Curso],
   migrations: ['src/database/migrations/**.ts'],
   synchronize: false,
 });
